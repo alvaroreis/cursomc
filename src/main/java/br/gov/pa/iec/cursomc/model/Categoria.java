0ae98770f79e23ce7a988f667ad3fff59345d07b
Alvaro Barros <alvaroluiz.rb@gmail.com>
2018-07-04 12:02:41 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/br/gov/pa/iec/cursomc/model/Categoria.java
+++ src/main/java/br/gov/pa/iec/cursomc/model/Categoria.java
@@ -5,14 +5,23 @@ package br.gov.pa.iec.cursomc.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author alvarobarros
  *
  */
+
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 6471684689262445696L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String nome;
